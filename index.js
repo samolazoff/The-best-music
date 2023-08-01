1a3ca4475e28eac6a2209c59a01889637a88b45a
unknown <samolazov.roman.alex@gmail.com>
2023-08-01 22:56:27 +0300

    ADD class css 'active fot link nav

--- index.js
+++ index.js
@@ -14,10 +14,14 @@ app.use(express.static( "public"));
 
 app.get('/', (req, res) => {
     const news=JSON.parse(fs.readFileSync('public/data/news.json', 'utf8'));
-    res.render('pages/index',{news}
+    res.render('pages/index',
+        {
+            news,
+            title: "News",
+            isHome: true
+        }
     );
 });
-
 app.listen(PORT, () => {
     console.log(`Server is runnig: ${PORT}`);
 });
