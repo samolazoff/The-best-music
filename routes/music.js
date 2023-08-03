ee167d5018cc358a3d4a69341758bb554481dc8a
Samolazoff <samolazov.roman.alex@gmail.com>
2023-08-03 19:27:26 +0300

    CREATE block music

--- routes/music.js
+++ routes/music.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
+import fs from 'fs';
 
 const router = Router();
 
 router.get('/music', (req, res) => {
+    const music=JSON.parse(fs.readFileSync('public/data/music.json', 'utf8'));
     res.render('pages/music',
         {
+            music,
             title: "Music",
             isMusic: true
         }
