5b219d236b61ca75fcfd625301b0ec49305d01db
unknown <samolazov.roman.alex@gmail.com>
2023-08-06 20:37:16 +0300

    CREATE: block add new user with conect DB

--- routes/login.js
+++ routes/login.js
@@ -1,5 +1,21 @@
 import { Router } from "express";
+import { Schema, model } from "mongoose";
 
+const userSchema = new Schema({
+    login: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+const User = model('User', userSchema);
 const router = Router();
 
 router.get('/login', (req, res) => {
@@ -10,10 +26,30 @@ router.get('/login', (req, res) => {
         }
     );
 });
-
 router.post('/login', (req,res) => {
     console.log(req.body);
     res.redirect('/')
-})
+});
+router.get('/autoriz', (req, res) => {
+    res.render('pages/autoriz',
+        {
+            title: "Add new user"
+        }
+    );
+});
+router.post('/autoriz', async (req, res) => {
+    const user = new User({
+        login: req.body.login,
+        email: req.body.email,
+        password: req.body.password
+    });
+    try {
+        await user.save();
+        console.log(req.body);
+        res.redirect('/')
+    } catch (error) {
+        console.log(error);
+    }
+});
 
 export default router;
