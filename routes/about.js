29dbccd0dbe0416202c77698c6aa3da620565695
unknown <samolazov.roman.alex@gmail.com>
2023-08-11 21:14:08 +0300

    CREATE block About(ADD function add path)

--- routes/about.js
+++ routes/about.js
@@ -1,14 +1,49 @@
 import { Router } from "express";
+import { Schema, model } from "mongoose";
 
 const router = Router();
 
-router.get('/about', (req, res) => {
+const pathSchema = new Schema({
+    titlePath: {
+        type: String,
+        required: true
+    },
+    desPath: {
+        type: String,
+        required: true
+    },
+});
+
+const Path = model('Path', pathSchema);
+
+router.get('/about', async (req, res) => {
+    const allPath = await Path.find(); 
     res.render('pages/about',
         {
+            allPath,
             title: "About",
             isAbout: true
         }
     );
 });
-
+router.post('/path/add', async (req, res) => {
+    const path = new Path({
+        titlePath: req.body.path,
+        desPath: req.body.pathDes
+    })
+    try {
+        await path.save();
+        res.redirect('/about')
+    } catch (error) {
+        console.log(error);
+    }
+});
+router.get('/path/add', async (req, res) => {
+    res.render('pages/addPath',
+        {
+            title: "Add new path",
+            isAbout: true
+        }
+    );
+});
 export default router;
